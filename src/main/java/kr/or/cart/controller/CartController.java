8f819fa062a7da1d7868d273ea396eae5d850fa7
mandooman <choisw1206@gmail.com>
2022-11-04 15:00:11 +0900

    -준석 장바구니 하는중

--- src/main/java/kr/or/cart/controller/CartController.java
+++ src/main/java/kr/or/cart/controller/CartController.java
@@ -1,13 +1,28 @@
 package kr.or.cart.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.cart.model.service.CartService;
+import kr.or.cart.model.vo.Cart;
+import kr.or.member.model.vo.Member;
+import kr.or.order.model.vo.Order;
 
 @Controller
 public class CartController {
 	@Autowired
 	private CartService service;
-
+	
+	@RequestMapping(value="/cart.do")
+	public String cart(Model model, Member m) {
+		ArrayList<Cart> list = service.selectAllCartList(m);
+		model.addAttribute("list",list);
+		System.out.println(m);
+		System.out.println(list);
+		return "myPage/cart";
+	}
 }
--- src/main/java/kr/or/cart/model/service/CartService.java
+++ src/main/java/kr/or/cart/model/service/CartService.java
@@ -1,11 +1,25 @@
 package kr.or.cart.model.service;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.cart.model.dao.CartDao;
+import kr.or.cart.model.vo.Cart;
+import kr.or.member.model.vo.Member;
+import kr.or.order.model.vo.Order;
 
 @Service
 public class CartService {
+	@Autowired
 	private CartDao dao;
+
+	public ArrayList<Cart> selectAllCartList(Member m) {
+		ArrayList<Cart> list = dao.selectAllCartList(m);
+		System.out.println(list);
+		System.out.println(m);
+		return (ArrayList<Cart>) list;
+	}
 
 }
